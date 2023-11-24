1169072607200c0edb83d426f3d7a23f56b21db7
Vijay34j <151818969+Vijay34j@users.noreply.github.com>
2023-11-24 17:03:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Save-Restricted-Bot")]]), reply_to_message_id=message.id)
+	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
@@ -253,12 +253,12 @@ __just send post/s link__
 
 **FOR PRIVATE CHATS**
 
-__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__
+**__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__**
 
 **FOR BOT CHATS**
 
-__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
+**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
 
 ```
 https://t.me/b/botusername/4321
@@ -266,7 +266,7 @@ https://t.me/b/botusername/4321
 
 **MULTI POSTS**
 
-__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+**__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
 
 ```
 https://t.me/xxxx/1001-1010
@@ -274,7 +274,7 @@ https://t.me/xxxx/1001-1010
 https://t.me/c/xxxx/101 - 120
 ```
 
-__note that space in between doesn't matter__
+**__note that space in between doesn't matter__**
 """
 
 
