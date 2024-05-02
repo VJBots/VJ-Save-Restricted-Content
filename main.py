18c562ea0d00054e63805b46b2f67bbfc7f219e1
cimraankhaan1 <103635804+cimraankhaan1@users.noreply.github.com>
2024-05-02 15:13:10 +0300

    main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def progress(current, total, message, type):
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/khaan_films")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
