b1052f321639dade05e6b8bff0baf59baba535c6
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-29 13:47:45 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -7,7 +7,34 @@ strings = {
     'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
 }
 
-HELP_TXT = 
+HELP_TXT = """**Help Menu** 
+
+__FOR PRIVATE CHATS__
+
+_first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link_
+
+
+__FOR BOT CHATS__
+
+_send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below_
+
+```
+https://t.me/b/botusername/4321
+```
+
+__MULTI POSTS__
+
+_send public/private posts link as explained above with formate "from - to" to send multiple messages like below_
+
+
+```
+https://t.me/xxxx/1001-1010
+
+https://t.me/c/xxxx/101 - 120
+```
+
+_note that space in between doesn't matter_"""
        
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
