0e0d10611e6f00bd6b28d647bd5e538223cc0e7e
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-29 16:18:57 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -7,25 +7,25 @@ strings = {
     'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
 }
 
-HELP_TXT = """**Help Menu** 
+HELP_TXT = """**🌟 Help Menu** 
 
-__FOR PRIVATE CHATS__
+**__FOR PRIVATE CHATS__**
 
-_first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link_
+__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__
 
 
-__FOR BOT CHATS__
+**__FOR BOT CHATS__**
 
-_send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below_
+__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
 
 ```
 https://t.me/b/botusername/4321
 ```
 
-__MULTI POSTS__
+**__MULTI POSTS__**
 
-_send public/private posts link as explained above with formate "from - to" to send multiple messages like below_
+__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
 
 
 ```
@@ -34,7 +34,7 @@ https://t.me/xxxx/1001-1010
 https://t.me/c/xxxx/101 - 120
 ```
 
-_note that space in between doesn't matter_"""
+__note that space in between doesn't matter__"""
        
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
