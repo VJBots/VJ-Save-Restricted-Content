b9353be72830852b4ee6d87f3997a771c7955735
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-29 11:54:11 +0530

    Add files via upload

---
+++ TechVJ/strings.py
@@ -0,0 +1,6 @@
+strings = {
+    'need_login': "U have to /login before using then bot can download restricted content ❕",
+    'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
+}
+    
+    
