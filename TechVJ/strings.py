3e572f078a9ebcfc1ff40213a836432daf1b10e5
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-29 11:57:43 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -1,6 +1,12 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 strings = {
     'need_login': "U have to /login before using then bot can download restricted content ❕",
     'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
 }
-    
-    
+       
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
