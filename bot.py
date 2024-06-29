efb3230e041733703ce5acebc6fc5a1a4dc4d63e
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-29 11:16:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,11 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from main import Bot
-from pyromod import listen
 
 Bot().run()
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
