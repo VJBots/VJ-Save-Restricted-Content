1321b73c5301d1a3070f411d447f7e77ce74b8d9
Heroku0007 <156164661+Heroku0007@users.noreply.github.com>
2024-07-21 17:34:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "26928239"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "eb5835a09739eba31d78971be7492d24")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb-srv://tnbots:tnbots@cluster0.Jkures.mongodb.net/?retryWrites=true&w=majority")
