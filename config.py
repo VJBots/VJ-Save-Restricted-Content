dbdfd2320c65ee84f9d56601fef9b6b6ab21c81c
DnxArafat <51267324+DnxArafat@users.noreply.github.com>
2024-09-20 20:51:24 +0600

    UpdateVJj config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("7531297522:AAGHKt2RYE2GLnTocUnxrNNSNsdU-woMQhM", "")
 
 #Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "24266722"))
