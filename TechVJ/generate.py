5597e93f20f4695cef6886168269115e3f02b1c5
Titusvirous <mizoramarjun@gmail.com>
2024-12-08 11:25:11 +0530

    Update generate.py

--- TechVJ/generate.py
+++ TechVJ/generate.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @LDRHACK
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @LDRHACK
 
 import traceback
 from pyrogram.types import Message
