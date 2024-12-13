534f936db8cf4de2ffeef0f7aeaf9ad0651882f7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-13 18:54:05 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,5 +1,43 @@
-from pymongo import MongoClient
-from config import DB_URI
+import motor.motor_asyncio
+from config import DB_NAME, DB_URI
 
-mongo_client = MongoClient(DB_URI)
-database = mongo_client.userdb.sessions
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            session = None,
+        )
+    
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id':int(id)})
+        return bool(user)
+    
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_session(self, id, session):
+        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
+
+    async def get_session(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user['session']
+
+db = Database(DB_URI, DB_NAME)
