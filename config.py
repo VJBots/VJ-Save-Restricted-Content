8c44c96bb51587efc3b8f0bb31e6dcd896e2fc78
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-13 18:51:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 
-#Bot token @Botfather
+# Bot token @Botfather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-#Your API ID from my.telegram.org
+# Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", ""))
 
-#Your API Hash from my.telegram.org
+# Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
-#Database 
+# Your Mongodb Database Url
 DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = environ.get("DB_NAME", "vjsavecontentbot")
