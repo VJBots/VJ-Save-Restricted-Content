582987ef17d559aae30d1882b6a840e5570d87f1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-13 19:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ API_ID = int(os.environ.get("API_ID", ""))
 # Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
+# Your Owner / Admin Id For Broadcast 
+ADMINS = int(os.environ.get("ADMINS", ""))
+
 # Your Mongodb Database Url
 DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "vjsavecontentbot")
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
