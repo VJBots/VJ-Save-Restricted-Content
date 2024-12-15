24912483ee513d5e3882a9434ba42203d9a88c90
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-15 06:27:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,3 +15,6 @@ ADMINS = int(os.environ.get("ADMINS", ""))
 # Your Mongodb Database Url
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
