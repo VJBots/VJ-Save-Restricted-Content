b21824a83140b1be1ea31a881adf6c9b56a48848
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-23 11:34:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,8 @@ API_HASH = os.environ.get("API_HASH", "")
 ADMINS = int(os.environ.get("ADMINS", "6073523936"))
 
 # Your Mongodb Database Url
-DB_URI = os.environ.get("DB_URI", "")
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
